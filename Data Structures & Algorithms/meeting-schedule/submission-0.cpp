5c1c241b859b2dd52460cfa16e6237564931e0b9
Chenghao Xie <112790673+SeamusXie03@users.noreply.github.com>
2026-05-07 15:01:34 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        vector<Interval> sorted_intervals = intervals;
+
+        // O(nlog(n))
+        sort(sorted_intervals.begin(), sorted_intervals.end(),
+            [](const Interval& p, const Interval& q) {
+                return p.start < q.start;
+            }
+        );
+
+        // O(n)
+        for (int idx = 1; idx < sorted_intervals.size(); idx++) {
+            if (sorted_intervals[idx].start < sorted_intervals[idx - 1].end) {
+                return false;
+            }
+        }        
+
+        return true;
+    }
+};
