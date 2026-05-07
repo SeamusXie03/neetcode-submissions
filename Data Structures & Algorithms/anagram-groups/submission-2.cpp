1ea7c7cc4b3433e45ad27bf3ecc2a6b726eab6d5
Chenghao Xie <112790673+SeamusXie03@users.noreply.github.com>
2026-05-07 16:24:40 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> keyWord;
+
+        for (auto& str : strs) {
+            vector<int> charCount(26, 0);
+
+            for (char& c : str) {
+                charCount[c - 'a']++;
+            }
+
+            string key = "";
+            for (int count : charCount) {
+                key += to_string(count) + "-";
+            }
+
+            keyWord[key].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (const auto& it : keyWord) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
