7f2b98f7b76961ac560d12ceb6a188e6a8741ee7
Chenghao Xie <112790673+SeamusXie03@users.noreply.github.com>
2026-05-08 21:42:00 -0700

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(nums.size(), 1);
+
+        // compute prefix product
+        int runProd = 1;
+        for (int idx = 0; idx < n; idx++) {
+            result[idx] = runProd;
+            runProd *= nums[idx];
+        }
+
+        runProd = 1;
+        // compute postfix product
+        for (int idx = n - 1; idx >= 0; idx--) {
+            result[idx] *= runProd;
+            runProd *= nums[idx];
+        }
+
+        return result;
+    }
+};
